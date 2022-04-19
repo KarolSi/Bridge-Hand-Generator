6efde98bf7e40b6d37adc7ae665893f4a96390ef
KarolS <karol@sicinscy.pl>
2022-04-19 16:57:04 +0200

    basic window

--- generator.py
+++ generator.py
@@ -1,56 +1,82 @@
 import random as rd
+import turtle 
 
-def wypisanie(zawo):
-    print('N:')
-    print("     ♠ ", end=" ")
-    for i in range(len(zawo["1"])):
-        print(zawo["1"][i], end=" ")
-    print("")
-    print("     ♥ ", end=" ")
-    for i in range(len(zawo["2"])):
-        print(zawo["2"][i], end=" ")
-    print("")
-    print("     ♦ ", end=" ")
-    for i in range(len(zawo["3"])):
-        print(zawo["3"][i], end=" ")
-    print("")
-    print("     ♣ ",end = " ")
-    for i in range(len(zawo["4"])):
-        print(zawo["4"][i], end=" ")
+SPos = turtle.Vec2D(-50, -150)
+NPos = turtle.Vec2D(-50, 150)
+WPos = turtle.Vec2D(-150,50)
+EPos = turtle.Vec2D(150, 50)
 
+def writing(pla):
+    
+    for i in range(len(pla["1"])):
+        print(pla["1"][i], end=" ")
+    print("")
+    
+    for i in range(len(pla["2"])):
+        print(pla["2"][i], end=" ")
+    print("")
+    
+    for i in range(len(pla["3"])):
+        print(pla["3"][i], end=" ")
+    print("")
+    
+    for i in range(len(pla["4"])):
+        print(pla["4"][i], end=" ")
 
-def losowanie(zawodnik):
+def draw(player):
         global zaw 
-        zaw = zawodnik
-        kolor = rd.randint(0,3)
+        zaw = player
+        color = rd.randint(0,3)
         karta = rd.randint(1,13)
 
-        if(karty[karta+13*kolor]!='#'):
-            zawodnik[str(kolor+1)].append(karty[karta+(13*kolor)])
-            karty[karta+13*kolor] = '#'
+        if(cards[karta+13*color]!='#'):
+            player[str(color+1)].append(cards[karta+(13*color)])
+            cards[karta+13*color] = '#'
         
             return 0
         else:
-            losowanie(zaw)
+            draw(zaw)
 
+
+
+#Window setting
+wn = turtle.Screen()
+wn.title("Bridge Hands Generator")
+wn.bgcolor('white')
+wn.register_shape('nesw.gif')
+
+
+#Shape setting
+nesw = turtle.Turtle()
+nesw.up()
+nesw.setpos(turtle.Vec2D(0, 0))
+nesw.shape('nesw.gif')
+
+
+#Writer setting
+writer = turtle.Turtle()
+writer.up()
+writer.ht()
 
 
 print("ile rozdań rozdać?")
 roz = int(input("<<"))
 
 for i in range(roz):
-    N = {'1': [],'2': [], '3': [], '4': []}
-    E = {'1': [],'2': [], '3': [], '4': []}
-    S = {'1': [],'2': [], '3': [], '4': []}
-    W = {'1': [],'2': [], '3': [], '4': []}
-    karty = [" ","2","3","4","5","6","7","8","9","T","J","Q","K","A","2","3","4","5","6","7","8","9","T","J","Q","K","A","2","3","4","5","6","7","8","9","T","J","Q","K","A","2","3","4","5","6","7","8","9","T","J","Q","K","A"]
+    N = {'1': ["♠ "],'2': ["♥ "], '3': ["♦ "], '4': ["♣ "]}
+    E = {'1': ["♠ "],'2': ["♥ "], '3': ["♦ "], '4': ["♣ "]}
+    S = {'1': ["♠ "],'2': ["♥ "], '3': ["♦ "], '4': ["♣ "]}
+    W = {'1': ["♠ "],'2': ["♥ "], '3': ["♦ "], '4': ["♣ "]}
+    cards = [" ","2","3","4","5","6","7","8","9","T","J","Q","K","A","2","3","4","5","6","7","8","9","T","J","Q","K","A","2","3","4","5","6","7","8","9","T","J","Q","K","A","2","3","4","5","6","7","8","9","T","J","Q","K","A"]
 
     for i in range(0,13):
-        losowanie(N)
-        losowanie(S)
-        losowanie(E)
-        losowanie(W)
-    wypisanie(N)
+        draw(N)
+        draw(S)
+        draw(E)
+        draw(W)
+    writing(N)
+    
+    
     
 
     
