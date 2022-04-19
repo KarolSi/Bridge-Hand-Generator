2e02e0783c5faa08b1312fe190008fd37c8bfef1
KarolS <karol@sicinscy.pl>
2022-04-19 13:20:56 +0200

    ready generator

--- generator.py
+++ generator.py
@@ -1,12 +1,45 @@
-import random
+import random as rd
+
+
+def losowanie(zawodnik):
+        global zaw 
+        zaw = zawodnik
+        kolor = rd.randint(0,3)
+        karta = rd.randint(1,13)
+
+        if(karty[karta+13*kolor]!='#'):
+            zawodnik[str(kolor+1)].append(karty[karta+(13*kolor)])
+            #print(karty[karta+13*kolor])
+            #print(kolor)
+            #print(karta)
+            karty[karta+13*kolor] = '#'
+            #print(karty)
+            return 0
+        else:
+            losowanie(zaw)
+
 
 
 print("ile rozdań rozdać?")
 roz = int(input("<<"))
 
-for i in roz:
+for i in range(roz):
+    N = {'1': [],'2': [], '3': [], '4': []}
+    E = {'1': [],'2': [], '3': [], '4': []}
+    S = {'1': [],'2': [], '3': [], '4': []}
+    W = {'1': [],'2': [], '3': [], '4': []}
+    karty = [" ","2","3","4","5","6","7","8","9","T","J","Q","K","A","2","3","4","5","6","7","8","9","T","J","Q","K","A","2","3","4","5","6","7","8","9","T","J","Q","K","A","2","3","4","5","6","7","8","9","T","J","Q","K","A"]
+
+    for i in range(0,13):
+        losowanie(N)
+        losowanie(S)
+        losowanie(E)
+        losowanie(W)
+    print(N)
+    print(S)
+    print(W)
+    print(E)
+
+
     
-    kara = [" ", "2", "3","4","5","6","7","8","9","T","J","Q","K","A"]
-    kiery = [" ", "2", "3","4","5","6","7","8","9","T","J","Q","K","A"]
-    trefle = [" ", "2", "3","4","5","6","7","8","9","T","J","Q","K","A"]
-    piki = [" ", "2", "3","4","5","6","7","8","9","T","J","Q","K","A"]
+    
